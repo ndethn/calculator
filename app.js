3d07f6aadc784018b7971021d8200516fd7361e6
Ethan <uhhhhhethan@gmail.com>
2023-01-25 17:10:11 -0800

    Add pseudo code for the add function and declare it

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
+// calculator project -- 1/25/23
+
 // declare basic math functions
 // add function
+/*
+add will be assigned to a new function with the parameter of values
+from the user
+return the sum of the values inputted by user
+*/
+
+const add = function (value) {
+    let sum = 0;
+    return sum += value;
+}
