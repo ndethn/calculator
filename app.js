896fd6b19e9ba6c13a971e0e922c32e1cf0f01e9
Ethan <uhhhhhethan@gmail.com>
2023-01-25 18:06:55 -0800

    Include basic math operators within if statements of the operate function

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 // calculator project -- 1/25/23
 
 // declare basic math functions
-let firstValue = parseInt(prompt('Enter first value: '));
-let secondValue = parseInt(prompt('Enter second value: '));
-
-console.log(firstValue + secondValue);
+// let firstValue = parseInt(prompt('Enter first value: '));
+// let secondValue = parseInt(prompt('Enter second value: '));
+// console.log(firstValue + secondValue);
 
 const add = (a, b) => {
     return a + b;
@@ -26,7 +25,15 @@ const divide = (a, b) => {
 // takes in an operator and two numbers and calls a math function
 
 const operate = (op, val1, val2) => {
-    if (op = 'add') {
+    if (op == add) {
         return add(val1, val2);
+    } else if (op == subtract) {
+        return subtract(val1, val2);
+    } else if (op == multiply) {
+        return multiply(val1, val2);
+    } else if (op == divide) {
+        return divide(val1, val2);
+    } else {
+        return 'error';
     }
 }
