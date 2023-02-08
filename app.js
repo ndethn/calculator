bb89e2de81ca62c3adddffd722b0d5f31f758fdf
Ethan <uhhhhhethan@gmail.com>
2023-02-07 21:11:59 -0800

    Create basic math functions

---
+++ app.js
@@ -0,0 +1,19 @@
+// calculator project 02/07
+
+// create basic math functions
+
+const add = (a, b) => {
+    return a + b;
+}
+
+const subtract = (a, b) => {
+    return a - b;
+}
+
+const multiply = (a, b) => {
+    return a * b;
+}
+
+const divide = (a, b) => {
+    return a / b;
+}
