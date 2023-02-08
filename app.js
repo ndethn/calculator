41312f453a521e0b894c4518de7dffa19e6ddca2
Ethan <uhhhhhethan@gmail.com>
2023-02-07 21:34:53 -0800

    Declare if statements within the operate function

--- app.js
+++ app.js
@@ -19,6 +19,16 @@ const divide = (a, b) => {
 }
 
 // create operate function
-const operate = (operator, num1, num2) => {
-
+const operate = (op, num1, num2) => {
+    if (op === 'add') {
+        return add(num1, num2);
+    } else if (op === 'subtract') {
+        return subtract(num1, num2);
+    } else if (op === 'multiply') {
+        return multiply(num1, num2);
+    } else if (op === 'divide') {
+        return divide(num1, num2);
+    } else {
+        return console.log('error');
+    }
 }
